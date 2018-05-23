d2d151ec2e5fa07c917dfb96c333ea4062075d89
openscript <1105080+openscript@users.noreply.github.com>
2018-05-23 15:47:44 +0200

    Show username and password after submit

--- src/app/containers/login/login.component.ts
+++ src/app/containers/login/login.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  currentUsername = '';
+  currentPassword = '';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(e: Event, username: string, password: string) {
+    e.preventDefault();
+    this.currentUsername = username;
+    this.currentPassword = password;
   }
 
 }
