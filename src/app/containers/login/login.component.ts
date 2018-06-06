36f287eb6602bee9687e974e2be9cc6603c3d101
Bladimir Ardiles <bladimir.ardilescruz@swisscom.com>
2018-06-06 12:53:00 +0200

    Form experiments. alerts and bindings.

--- src/app/containers/login/login.component.ts
+++ src/app/containers/login/login.component.ts
@@ -6,18 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  currentUsername = '';
-  currentPassword = '';
+  passwordField = '';
+  emailField = '';
+  strongPassword  = false;
+  emailValid = false;
+  emailEmpty = true;
+  passwordEmpty = true;
+
 
   constructor() { }
 
   ngOnInit() {
+    this.formProperlyFilledIn();
+  }
+
+  formProperlyFilledIn() {
+    console.log(this.emailValid && this.strongPassword);
+    return this.emailValid && this.strongPassword;
+  }
+
+  UpdateEmail(evt) {
+    evt.target.value.length > 0 ? this.emailEmpty = false : this.emailEmpty = true;
+    const userInput = evt.target.value;
+    this.emailValid =  userInput.indexOf('@') !== -1 && userInput.indexOf('.') !== -1;
+
+    return this.emailValid;
+  }
+
+  UpdatePassword(evt) {
+    evt.target.value.length > 0 ? this.passwordEmpty = false : this.passwordEmpty = true;
+    this.strongPassword = evt.target.value.length > 8;
+
+    return this.strongPassword;
   }
 
   onSubmit(e: Event, username: string, password: string) {
     e.preventDefault();
-    this.currentUsername = username;
-    this.currentPassword = password;
+    this.passwordField = password;
+    this.emailField = username;
   }
-
 }
